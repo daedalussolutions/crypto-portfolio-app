a6b3f396392caa6e1aadc722d518194267518eb2
daedalussolutions <dylancassell000@hotmail.com>
2023-11-29 17:27:03 -0500

    Coin Market Cap call added in currency.rb

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,2 +1,19 @@
 class Currency < ApplicationRecord
+    def calculate_value(amount)
+        (current_price.to_f * amount.to_f).round(2)
+    end
+    def current_price
+        url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest"
+        symbol = self.currency_symbol
+        query = {
+            "symbol" => symbol
+        }
+        headers = {
+            "X-CMC_PRO_API_KEY" => Rails.application.credentials.dig(:coin_market_cap, :api_key),
+            "Accepts" => "application/json"
+        }
+        request = HTTParty.get(url, :headers => headers, :query => query)
+        puts request
+        response = JSON.parse(request.body)["data"][symbol]["quote"]["CAD"]["price"]
+    end
 end
